55a936d6580677c3c5d5e867a2c8ae0504473a2b
Andrew <travers.drew@gmail.com>
2020-12-04 10:51:47 -0500

    Connect page content

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import {Element} from 'react-scroll';
+import { FaLinkedin, FaGithub } from 'react-icons/fa'
+import { IconContext } from 'react-icons/lib';
 
 const Contact = () => {
     return (
         <Element name='connect-section'>
             <div className='contact-container detail-container'>
-                <h1>Contact Me</h1>
+                <div className='contact-p'>
+                    <h3>
+                    You can find my work and contact information below at LinkedIn and Github. I look forward to connecting with you!
+                    </h3>
+                </div>
+                <div className='contact-links'>
+                    <IconContext.Provider value={{ size: '8vw' }}>
+                        <div>
+                            <FaLinkedin />
+                        </div>
+                        <div>
+                            <FaGithub />
+                        </div>
+                    </IconContext.Provider>
+                </div>
             </div>
         </Element>
     )
