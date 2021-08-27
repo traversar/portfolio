bb5cda1965099df8f66b2ed064b33541248d1d19
Andrew <travers.drew@gmail.com>
2021-08-27 17:40:24 -0400

    Mobile & desktop styling & project images

--- src/components/ProjectDetail.js
+++ src/components/ProjectDetail.js
@@ -9,13 +9,16 @@ const ProjectDetail = ({
             <div className='projectdetail-details-container'>
                 {projects.map((project, idx) => (
                     <div onClick={() => window.location=project.url} key={idx} className='projectdetail-details'>
-                        <h2>{project.name}</h2>
-                        <h3>{project.summary}</h3>
-                        <ul>
-                            {project.technologies.map((technology, idx) => (
-                                <li key={idx}>{technology}</li>
-                            ))}
-                        </ul>
+                        <img className='projectdetail-snapshot-img' src={project.img} alt='Project Snapshot'></img>
+                        <div>
+                            <h2>{project.name}</h2>
+                            <h3>{project.summary}</h3>
+                            <ul>
+                                {project.technologies.map((technology, idx) => (
+                                    <li key={idx}>{technology}</li>
+                                ))}
+                            </ul>
+                        </div>
                     </div>
                 ))}
             </div>
--- src/projectDetails.js
+++ src/projectDetails.js
@@ -1,20 +1,28 @@
+import writemeinSnap from './images/writemein.png';
+import pypracticeSnap from './images/pypractice.png';
+import astrologueSnap from './images/astrologue.png';
+
+
 export const projects = [
     {
         name: 'PyPractice',
         url: 'https://pypypractice.herokuapp.com',
-        summary: 'Python practice problems and challenges with interative Python interpreter.',
-        technologies: ['React', 'Redux', 'Flask', 'MaterialUI', 'SQLAlchemy', 'Docker'],
+        summary: 'Python practice problems and challenges with embedded Python interpreter.',
+        technologies: ['React/Redux', 'Flask', 'MaterialUI', 'SQLAlchemy', 'Docker'],
+        img: pypracticeSnap,
     },
     {
         name: 'Write Me In',
         url: 'https://writemein.herokuapp.com',
         summary: 'Collaborative storywriting - edit, contribute and browse user created stories.',
-        technologies: ['React/Redux', 'Express', 'Sequelize', 'PostgreSQL', 'CSS3']
+        technologies: ['React/Redux', 'Express', 'Sequelize', 'PostgreSQL', 'CSS3'],
+        img: writemeinSnap
     },
     {
         name: 'Astrologue',
         url: 'https://astrologue.herokuapp.com',
-        summary: 'Render astronomical chart data with attractive presentation, based on specified time and location.',
-        technologies: ['React/Redux', 'Django', 'PostgreSQL', 'CSS3', 'Docker']
+        summary: 'Render astronomical chart data with interactive presentation, based on specified time and location.',
+        technologies: ['React/Redux', 'Django', 'PostgreSQL', 'CSS3', 'Docker'],
+        img: astrologueSnap
     }
 ]
